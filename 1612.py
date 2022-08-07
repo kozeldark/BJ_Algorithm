1481edc9a98363f5748f14b6de2b47daff6c503a
Chanhee Oh <77625823+kozeldark@users.noreply.github.com>
2022-08-07 19:17:17 +0900

    Update 1612.py

--- 1612.py
+++ 1612.py
@@ -1,5 +1,5 @@
 N = int(input())
-div = 1
+div = 0
 
 if N % 2 == 0 or N % 5 == 0:
     print(-1)
@@ -7,5 +7,5 @@ else:
     for i in range(1, N+1):
         div = (10*div + 1) % N
         if div == 0:
-            print(i+1)
+            print(i)
             break
