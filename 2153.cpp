90f5dc4dbb25995fcaf28d0eb6829781b601f6e5
Chanhee Oh <77625823+kozeldark@users.noreply.github.com>
2022-08-18 02:49:08 +0900

    Add files via upload

---
+++ 2153.cpp
@@ -0,0 +1,54 @@
+﻿// ConsoleApplication162.cpp : 콘솔 응용 프로그램에 대한 진입점을 정의합니다.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <vector>
+#include <queue>
+#include <iostream>
+#include <algorithm>
+#include <functional>
+#include <cmath>
+#include <map>
+#include<memory.h>
+#include <set>
+
+using namespace std;
+
+int N;
+int sum = 0;
+string str;
+bool IsPrime(int num)
+{
+	int lcnt = 0;
+	for (lcnt = 2; lcnt < num; lcnt++)
+	{
+		if ((num % lcnt) == 0)
+			return false;
+	}
+	return true;
+}
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	
+	cin >> str;
+
+	for (int i = 0; i < str.length(); i++)
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+			sum += str[i] - 96;
+
+		else
+			sum += str[i] - 38;
+	}
+
+	if (IsPrime(sum))
+		cout << "It is a prime word.";
+	else
+		cout << "It is not a prime word.";
+
+	return 0;
+}
---
+++ 2156.cpp
@@ -0,0 +1,50 @@
+﻿// ConsoleApplication162.cpp : 콘솔 응용 프로그램에 대한 진입점을 정의합니다.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <vector>
+#include <queue>
+#include <iostream>
+#include <algorithm>
+#include <functional>
+#include <cmath>
+#include <map>
+#include<memory.h>
+#include <set>
+
+
+using namespace std;
+
+int N;
+vector<int> v;
+vector<int> dp;
+
+
+int main() 
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> N;
+
+	for (int i = 0; i < 3; i++)
+	{
+		v.push_back(0);
+		dp.push_back(0);
+	}
+
+	for (int i = 0; i < N; i++)
+	{
+		int temp;
+		cin >> temp;
+		v.push_back(temp);
+	}
+
+	for (int i = 3; i < N + 3; i++)
+		dp.push_back(max(max(dp[i - 3] + v[i - 1] + v[i], dp[i - 2] + v[i]), dp[i - 1]));
+
+	cout << dp.back();
+
+}
