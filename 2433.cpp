64e144c5af708cde8d5f159460229fdfe79896aa
Chanhee Oh <77625823+kozeldark@users.noreply.github.com>
2022-08-23 23:58:07 +0900

    Add files via upload

---
+++ 2433.cpp
@@ -0,0 +1,75 @@
+﻿// ConsoleApplication162.cpp : 콘솔 응용 프로그램에 대한 진입점을 정의합니다.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <vector>
+#include <queue>
+#include <iostream>
+#include <algorithm>
+#include <functional>
+#include <cmath>
+#include <map>
+#include<memory.h>
+#include <set>
+
+using namespace std;
+
+int n, m, c;
+vector<int> v;
+vector<int> ans;
+priority_queue <int> pq;
+multiset<int> ms;
+//bool ans = false;
+int mx = 0;
+int mn = 10001;
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n >> m >> c;
+
+	for (int i = 0; i < n; i++)
+	{
+		int tmp;
+		cin >> tmp;
+		v.push_back(tmp);
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		ms.insert(v[i]);
+
+		if (i > m - 1)
+			ms.erase(ms.find(v[i - m]));
+
+		if (i >= m - 1)
+		{
+			//int max = *max_element(v.begin() + i, v.end() + i + m);
+			//int min = *min_element(v.begin() + i, v.end() + i + m);
+
+			mx = *(--ms.end());
+			mn = (*ms.begin());
+
+			if (mx - mn <= c)
+			{
+				//cout << i << "\n";
+				ans.push_back(i - m + 2);
+			}
+		}
+	}
+
+	if (ans.size() != 0)
+	{
+		for(int i=0; i < ans.size(); i++)
+			cout << ans[i] << "\n";
+	}
+	else
+		cout << "NONE" << "\n";
+
+
+	return 0;
+}
---
+++ 2436.cpp
@@ -0,0 +1,58 @@
+﻿// ConsoleApplication162.cpp : 콘솔 응용 프로그램에 대한 진입점을 정의합니다.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <vector>
+#include <queue>
+#include <iostream>
+#include <algorithm>
+#include <functional>
+#include <cmath>
+#include <map>
+#include<memory.h>
+
+using namespace std;
+
+int A, B;
+int ans = 0;
+int a_tmp, b_tmp;
+
+ int gcd(int a, int b)
+{
+	if (b == 0)
+		return a;
+	else
+		return gcd(b, a % b);
+}
+ /*
+ int lcm(int a, int b)
+ {
+	 return a * b / gcd(a, b);
+ }
+ */
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> A >> B;
+	B = B / A;
+
+	for (int i = 1; i <= sqrt(B); i++)
+	{
+		if (B % i == 0)
+		{
+			if (gcd(i, B / i) == 1)
+			{
+				a_tmp = i, b_tmp = B / i;
+
+			}
+		}
+	}
+	cout << a_tmp * A << ' ' << b_tmp * A;
+
+	return 0;
+}
